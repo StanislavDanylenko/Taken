7681e26ab1030e41be2ac2ca270d6292fea43f8f
Stanislav Danylenko <stanislav.danylenko@gmail.com>
2025-03-13 23:51:58 +0200

    Fix text alignment

--- app/src/main/java/stanislav/danylenko/taken/activity/InfoActivity.java
+++ app/src/main/java/stanislav/danylenko/taken/activity/InfoActivity.java
@@ -2,7 +2,10 @@ package stanislav.danylenko.taken.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.text.LineBreaker;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import stanislav.danylenko.taken.R;
 
@@ -12,5 +15,10 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            TextView info = findViewById(R.id.info);
+            info.setJustificationMode(LineBreaker.JUSTIFICATION_MODE_INTER_WORD);
+        }
     }
 }
